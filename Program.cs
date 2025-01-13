2179acdcc0d60208a3e3b82c6306cc68b1543655
Akanksha Tiwari <148522346+akanksha0405@users.noreply.github.com>
2025-01-13 14:58:51 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace akanksha123
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           // float num=Convert.ToSingle(Console.ReadLine());
+           int num=Convert.ToInt32(Console.ReadLine());
+            string mem=Console.ReadLine();
+            int FinalAmt;
+        
+            if (mem == "GoldM")
+            {
+                FinalAmt = num - (num*20/100);
+                Console.WriteLine("Discount of 20% final amount:{0}",FinalAmt );
+            }
+            else if (mem == "SilverM")
+                    {
+               FinalAmt = num - (num *10/100);
+                Console.WriteLine("discount of 10%.final amount:{0}",FinalAmt);     
+            }
+            else
+            {
+                Console.WriteLine("No discount");
+            }
+        }
+    }
+}
