151d9ac56f9e97b7a8b7a5917410c839d87180bf
Akanksha Tiwari <148522346+akanksha0405@users.noreply.github.com>
2025-01-13 15:23:23 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
+using System.Runtime.Remoting.Services;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,24 +12,23 @@ namespace akanksha123
     {
         static void Main(string[] args)
         {
-           // float num=Convert.ToSingle(Console.ReadLine());
-           int num=Convert.ToInt32(Console.ReadLine());
-            string mem=Console.ReadLine();
-            int FinalAmt;
-        
-            if (mem == "GoldM")
+            Console.WriteLine("ENTER THE NUMBER");
+            int num=Convert.ToInt32(Console.ReadLine());
+            
+            if (num > 0) {
+                if (num % 2 == 0) {
+                    Console.WriteLine("number is positive and its even also");
+                }
+                else {
+                    Console.WriteLine("number is positive and odd also");
+                } }
+            else if (num < 0)
             {
-                FinalAmt = num - (num*20/100);
-                Console.WriteLine("Discount of 20% final amount:{0}",FinalAmt );
-            }
-            else if (mem == "SilverM")
-                    {
-               FinalAmt = num - (num *10/100);
-                Console.WriteLine("discount of 10%.final amount:{0}",FinalAmt);     
+                Console.WriteLine("number is negative");
             }
             else
             {
-                Console.WriteLine("No discount");
+                Console.WriteLine("number is Zero");
             }
         }
     }
